8f90e10c6afc332c35ada36d86a48068751af142
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-09-10 15:43:46 +0200

    🚨 Fix auth errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
 
@@ -14,14 +14,14 @@ import { JwtStrategy } from './jwt.strategy';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (ConfigService: ConfigService) => ({
-        secret: ConfigService.get<string>('JWT_SECRET'),
+        secret: ConfigService.getOrThrow<string>('JWT_SECRET'),
         signOptions: { expiresIn: '2d' },
         global: true,
       }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, JwtService, ConfigService],
+  providers: [AuthService, JwtStrategy, ConfigService],
   exports: [AuthModule],
 })
 export class AuthModule {}
